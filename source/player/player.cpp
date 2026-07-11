2dca6e6002202f3e3a32de272202c0f7dd741746
Bradly Landucci <salvesoundsofficial@gmail.com>
2026-07-10 22:23:35 -0700

    Added banana boy walk left/right assets

--- source/player/player.cpp
+++ source/player/player.cpp
@@ -1,5 +1,7 @@
 #include <player/player.h>
 
+#include <input/keyboard.h>
+
 //------------------------------------------------------------------//
 
 Player::Player()
@@ -11,25 +13,41 @@ Player::Player()
     addChild(collision());
     addChild(mp_sprite);
 
-    auto idle{ std::make_shared<AE::Spritesheet>("assets/idle.png", 1, 1, 1, 1, true) };
-    const int numFrames{ 24 };
-    const int rows{ 4 };
-    const int columns{ 6 };
+    const int numFrames{ 4 };
+    const int rows{ 1 };
+    const int columns{ 4 };
     const int fps{ 8 };
-    const bool loops{ false };
-    auto walkLeft{ std::make_shared<AE::Spritesheet>("assets/walk_left.png", numFrames, rows, columns, fps, loops) };
-    auto walkRight{ std::make_shared<AE::Spritesheet>("assets/walk_right.png", numFrames, rows, columns, fps, loops) };
+    const bool loops{ true };
+    auto idle{ std::make_shared<AE::Spritesheet>("assets/banana_boy_walk_right.png", numFrames, rows, columns, fps,
+                                                 loops) };
+    auto walkLeft{ std::make_shared<AE::Spritesheet>("assets/banana_boy_walk_left.png", numFrames, rows, columns, fps,
+                                                     loops) };
+    auto walkRight{ std::make_shared<AE::Spritesheet>("assets/banana_boy_walk_right.png", numFrames, rows, columns, fps,
+                                                      loops) };
 
     mp_sprite->addAnimation("idle", idle);
     mp_sprite->addAnimation("walkLeft", walkLeft);
     mp_sprite->addAnimation("walkRight", walkRight);
+
+    mp_sprite->playAnimation("idle");
 }
 
 //------------------------------------------------------------------//
 
 void Player::physicsUpdate(double deltaTime)
 {
-    mp_sprite->playAnimation("idle");
+    if (AE::Keyboard::isPressed(AE::Keyboard::Key::Right))
+    {
+        mp_sprite->playAnimation("walkRight");
+    }
+    else if (AE::Keyboard::isPressed(AE::Keyboard::Key::Left))
+    {
+        mp_sprite->playAnimation("walkLeft");
+    }
+    else
+    {
+        // mp_sprite->playAnimation("idle");
+    }
 }
 
 //------------------------------------------------------------------//
